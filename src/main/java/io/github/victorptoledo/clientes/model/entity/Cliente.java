c7ad473ef70aba4300940677330a429ac9d259b3
Victor Toledo <victorptoledo@outlook.com>
2022-07-01 12:39:18 -0300

    Criação das entitys cliente e serviço

---
+++ src/main/java/io/github/victorptoledo/clientes/model/entity/Cliente.java
@@ -0,0 +1,32 @@
+package io.github.victorptoledo.clientes.model.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column
+    private LocalDate dataCadastro;
+
+    @PrePersist
+    public void PrePersist(){
+        setDataCadastro(LocalDate.now());
+    }
+}
