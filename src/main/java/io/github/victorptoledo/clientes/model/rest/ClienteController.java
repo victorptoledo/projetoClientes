4ee427295040bbb3f4198e94e3481d2c7efbe9b0
Victor Toledo <victorptoledo@outlook.com>
2022-07-01 13:45:44 -0300

    Adição da funcionalidade de obter detalhes do cliente por ID

--- src/main/java/io/github/victorptoledo/clientes/model/rest/ClienteController.java
+++ src/main/java/io/github/victorptoledo/clientes/model/rest/ClienteController.java
@@ -5,6 +5,7 @@ import io.github.victorptoledo.clientes.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -21,5 +22,9 @@ public class ClienteController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente salvar(@RequestBody Cliente cliente) {
         return repository.save(cliente);
+    }
+    @GetMapping("{id}")
+    public Cliente acharPorId( @PathVariable Integer id) {
+        return repository.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
